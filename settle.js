c4f7a818f9648b21b2c6ec19f59f647a7547b960
Boolafish <boolafish945@gmail.com>
2016-11-17 13:53:16 +0800

    fix settle logic to clear 6/1~3 datas

--- settle.js
+++ settle.js
@@ -3,45 +3,68 @@
 const MongoClient = require('mongodb').MongoClient;
 const _ = require('lodash');
 
-const mongodbUrl = 'mongodb://localhost:27017/ach';
+const mongodbUrl = 'mongodb://140.112.29.42:27017/ach';
+
 
 let collection;
 let db;
 
+const currentDay = '01050603';
+const previousDay = '01050602';
+
+const initBankInBankList = function initBankInBankList(bank, bankList) {
+  const updatedBankList = bankList;
+  if (!(bank in updatedBankList)) {
+    updatedBankList[bank] = {
+      sc: 0.0,
+      sd: 0.0,
+      net: 0.0,
+    };
+  }
+  return updatedBankList;
+};
+
 MongoClient.connect(mongodbUrl, (err, database) => {
   db = database;
   collection = db.collection('tx');
-  collection.find({ P_TDATE: '01050601' }).toArray((error, docs) => {
+  collection.find({
+    $or: [{ P_TDATE: currentDay }, { P_TDATE: previousDay, P_TYPE: 'N' }],
+  }).toArray((error, docs) => {
     if (err) {
       console.error(`error: ${error}`);
     } else {
-      const bankList = {};
+      let bankList = {};
       let proposer;
+      let receiver;
       let amount;
       for (const tx of docs) {
         proposer = tx.P_PBANK.substring(0, 3);
-        amount = parseFloat(tx.P_AMT.substring(1, 11), 10);
-        if (proposer === 'A0D') {
-          console.log(`${tx.P_PBANK}, ${tx.P_TYPE}, ${tx.P_TXTYPE}, ${amount}, ${tx.P_AMT}`);
-        }
-        if (!(proposer in bankList)) {
-          bankList[proposer] = { sc: 0.0, sd: 0.0, net: 0.0 };
-        }
-        if (tx.P_TYPE === 'N' && tx.P_TXTYPE === 'SC') {
-          bankList[proposer].sc -= amount;
-        } else if (tx.P_TYPE === 'N' && tx.P_TXTYPE === 'SD') {
-          bankList[proposer].sd -= amount;
-        } else if (tx.P_TYPE === 'R' && tx.P_TXTYPE === 'SC') {
-          bankList[proposer].sc += amount;
-        } else if (tx.P_TYPE === 'R' && tx.P_TXTYPE === 'SD') {
-          bankList[proposer].sd += amount;
-        } else {
-          console.log('anomally!');
+        receiver = tx.P_RBANK.substring(0, 3);
+        amount = parseFloat(tx.P_AMT);
+
+        bankList = initBankInBankList(proposer, bankList);
+        bankList = initBankInBankList(receiver, bankList);
+
+        if (tx.P_TDATE === previousDay) {
+          if (tx.P_TYPE === 'N' && tx.P_TXTYPE === 'SD') {
+            bankList[proposer].sd += amount;
+            bankList[receiver].sd -= amount;
+          }
+        } else if (tx.P_TDATE === currentDay) {
+          if (tx.P_TXTYPE === 'SC') {
+            bankList[proposer].sc -= amount;
+            bankList[receiver].sc += amount;
+          } else if (tx.P_TYPE === 'R' && tx.P_TXTYPE === 'SD') {
+            bankList[proposer].sd += amount;
+            bankList[receiver].sd -= amount;
+          }
         }
       }
+
       for (const bankKey of _.keys(bankList)) {
         bankList[bankKey].net = bankList[bankKey].sc + bankList[bankKey].sd;
       }
+
       console.log(bankList);
       console.log(docs.length);
       db.close();
