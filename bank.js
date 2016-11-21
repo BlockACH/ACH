489e776c1b94a48996ed6205f92f3e934258265b
Boolafish <boolafish945@gmail.com>
2016-11-21 13:21:29 +0800

    reuse bank object in BankManager

--- bank.js
+++ bank.js
@@ -8,27 +8,19 @@ const config = require('./config.json');
 
 const gcoin = new GcoinPresenter(config.gcoin);
 
-const BANK_LIST = [
-  '6AB', 'A28', '46E', 'DD3', '822', 'CCC', '219',
-  '18C', '170', 'B63', '62F', '5E0', '666', '519',
-  'BA4', '5BD', '682', 'E07', 'B31', '0B1', 'FCB',
-  'B89', '101', 'EDB', 'E75', '75D', 'A0D', '22D',
-  'AB5', 'A1D', 'F73', 'C45', '481', '49A', 'EE0',
-  '269', '7BA', '48C', 'E0C', 'CE3', '8DA', '552',
-  '1F6', 'B30', '6D4', 'FB4', '4AD', '940', '838',
-  'E15', 'F8E', '717', 'C72', '882', 'EA0',
-];
-
 class BankManager {
   constructor() {
-    this.bankList = BANK_LIST;
+    this.bankList = config.bankList;
+    this.bankCache = {};
   }
 
   getBankByID(bankID) {
-    if (this.bankList.includes(bankID)) {
-      return new Bank(bankID);
+    // EA0 is a shared bank account for all the other unlisted banks.
+    const targetBankID = this.bankList.includes(bankID) ? bankID : 'EA0';
+    if (!(targetBankID in this.bankCache)) {
+      this.bankCache[targetBankID] = new Bank(targetBankID);
     }
-    return new Bank('EA0');
+    return this.bankCache[targetBankID];
   }
 }
 
